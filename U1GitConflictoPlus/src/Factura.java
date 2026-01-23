68406d780e116b78c8d387ac97b83faf19846878
Santiago_Marinez <20253ds099@utez.edu.mx>
2026-01-23 09:10:42 -0600

    Resuelto

--- U1GitConflictoPlus/src/Factura.java
+++ U1GitConflictoPlus/src/Factura.java
@@ -1,6 +1,6 @@
 public class Factura {
 
-    public static final String VERSION_OBJETIVO = "PENDIENTE";
+    public static final String VERSION_OBJETIVO = "RESUELTO";
 
     private static final double IVA = 0.16;
     private static final double PRECIO_UNITARIO = 1200.0;
